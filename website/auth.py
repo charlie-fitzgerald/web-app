9070a78df1d40e0a4011dc1b76b68918b690fc27
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2024-09-17 15:16:01 -0700

    Sign up form complete. Can save users to db. Fixed formatting in base.html

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 auth = Blueprint('auth', __name__)
 
@@ -14,20 +18,25 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash("Email more than 4", category="error")
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("First more than 2", category="error")
         elif password1 != password2:
             flash("password must match", category="error")
         elif len(password1) < 7:
             flash("password more than 7", category="error")
         else:
-            #add user to database
+            # add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='scrypt'))
+            db.session.add(new_user) 
+            db.session.commit()
             flash("Account created", category="success")
+            return redirect(url_for('views.home'))
+
 
     return render_template("sign_up.html")
